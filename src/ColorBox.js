a9f26dda6596a1c2da38115059002a8748aec7df
Alan Xavier <alan_xvr@hotmail.com>
2019-07-20 20:27:34 -0400

    Add colorHelpers.js to generate color levels

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import './ColorBox.css';
+import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./ColorBox.css";
 
 class ColorBox extends Component {
   constructor(props) {
     super(props);
-    this.state = { copied: false }
+    this.state = { copied: false };
     this.changeCopyState = this.changeCopyState.bind(this);
   }
   changeCopyState() {
@@ -15,23 +15,26 @@ class ColorBox extends Component {
   }
   render() {
     const { name, background } = this.props;
-    const {copied} = this.state;
-    return(
-      <CopyToClipboard text={background} onCopy={this.changeCopyState} >
-        <div style={{ background: background }} className='ColorBox'>
-          <div style={{ background: background }} className={`copy-overlay ${copied && 'show'}`}></div>
-          <div className={`copy-msg ${copied && 'show'}`}>
+    const { copied } = this.state;
+    return (
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{ background: background }} className="ColorBox">
+          <div
+            style={{ background: background }}
+            className={`copy-overlay ${copied && "show"}`}
+          />
+          <div className={`copy-msg ${copied && "show"}`}>
             <h1>Copied!</h1>
             <p>{background}</p>
           </div>
 
-          <div className='copy-container'>
-            <div className='box-content'>
+          <div className="copy-container">
+            <div className="box-content">
               <span>{name}</span>
             </div>
-            <button className='copy-button'>Copy</button>
+            <button className="copy-button">Copy</button>
           </div>
-          <span className='see-more'>More</span>
+          <span className="see-more">More</span>
         </div>
       </CopyToClipboard>
     );
---
+++ src/colorHelpers.js
@@ -0,0 +1,52 @@
+import chroma from "chroma-js";
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+function generatePalette(starterPalette) {
+  //Generate a NEW palette object
+  let newPalette = {
+    paletteName: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
+    colors: {}
+  };
+  // Creates an OBJECT in 'colors' object that is an empty array, eg. 50: [], 100: [], etc.
+  for (let level of levels) {
+    newPalette.colors[level] = [];
+  }
+  // Generates a scale for EACH color in the 'colors' object and assigns the lightest to darkest from 50 to 900
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, levels.length).reverse();
+    for(let i in scale) {
+      newPalette.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, '-'),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i]).css().replace('rgb', 'rgba').replace(')', ',1.0)')
+      });
+    }
+  }
+  return newPalette;
+}
+
+// Generates and array with 3 hex colors from Darkest to white
+function getRange(hexColor) {
+  const end = "#fff";
+  return [
+    chroma(hexColor)
+      .darken(1.4)
+      .hex(),
+    hexColor,
+    end
+  ];
+}
+
+// Maps the input colors (.scale) to a range of colors (.colors)
+function getScale(hexColor, numberOfColors) {
+  return chroma
+    .scale(getRange(hexColor))
+    .mode("lab")
+    .colors(numberOfColors);
+}
+
+export { generatePalette };
