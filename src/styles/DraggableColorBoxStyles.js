a794ee2f952c7207e93ede85dc405a1984c3f656
Alan Xavier <alan_xvr@hotmail.com>
2019-07-25 19:51:07 -0400

    Move styles to separate files and add contants file

---
+++ src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,34 @@
+const styles = {
+  root: {
+    width: "20%",
+    height: "25%",
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-4px",
+    "&:hover svg": {
+      color: "white",
+      transform: "scale(1.5)"
+    }
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    textTransform: "uppercase",
+    letterSpacing: "1px",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  deleteIcon: {
+    color: "rgba(0,0,0,0.5)",
+    transition: "all 0.2s ease-in-out"
+  }
+};
+
+export default styles;
