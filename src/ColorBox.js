911b430de15ab77094187164225393c4712fc1cd
Alan Xavier <alan_xvr@hotmail.com>
2019-07-28 19:51:16 -0400

    Add classnames node package to ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/ColorBoxStyles";
 
@@ -30,10 +31,15 @@ class ColorBox extends Component {
         <div style={{ background: background }} className={classes.ColorBox}>
           <div
             style={{ background: background }}
-            className={`${classes.copyOverlay} ${copied &&
-              classes.showOverlay}`}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied
+            })}
           />
-          <div className={`${classes.copyMsg} ${copied && classes.showMsg}`}>
+          <div
+            className={classNames(classes.copyMsg, {
+              [classes.showMsg]: copied
+            })}
+          >
             <h1>Copied!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
